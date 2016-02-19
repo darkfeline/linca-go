dc693e653036bc74dee739da2460a5fe005d8c64
Allen Li <darkfeline@felesatra.moe>
2016-02-18 22:47:19 -0800

    Fix pathing

--- linca.go
+++ linca.go
@@ -5,18 +5,20 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path"
 	"strings"
 )
 
 // Represents inotify event returned from inotifywait.
 type notifyEvent struct {
-	events []string
-	file   string
+	watched string
+	events  []string
+	file    string
 }
 
 // Make new notifyEvent.
-func newEvent(events []string, file string) *notifyEvent {
-	return &notifyEvent{events, file}
+func newEvent(watched string, events []string, file string) *notifyEvent {
+	return &notifyEvent{watched, events, file}
 }
 
 // Check if notify event contains an event of the given type.
@@ -41,7 +43,7 @@ func mkdirp(dir string) error {
 // Watch directory with inotify and send events over channel.
 func watcher(watchdir string, out chan<- *notifyEvent) {
 	defer close(out)
-	cmd := exec.Command("inotifywait", "-m", "--format", "%e\n%f", watchdir)
+	cmd := exec.Command("inotifywait", "-m", "--format", "%w\n%e\n%f", watchdir)
 	output, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatalf("Error trying to watch %s: %s", watchdir, err)
@@ -49,12 +51,16 @@ func watcher(watchdir string, out chan<- *notifyEvent) {
 	scanner := bufio.NewScanner(output)
 	cmd.Start()
 	for scanner.Scan() {
+		watched := scanner.Text()
+		if !scanner.Scan() {
+			log.Fatal("Incomplete output from inotifywait")
+		}
 		events := scanner.Text()
 		if !scanner.Scan() {
 			log.Fatal("Incomplete output from inotifywait")
 		}
 		file := scanner.Text()
-		event := newEvent(strings.Split(events, ","), file)
+		event := newEvent(watched, strings.Split(events, ","), file)
 		out <- event
 	}
 	if err := scanner.Err(); err != nil {
@@ -69,9 +75,10 @@ func linker(destdir string, events <-chan *notifyEvent) {
 			// This is an event we don't care about
 			continue
 		}
-		fi, err := os.Stat(event.file)
+		file := path.Join(event.watched, event.file)
+		fi, err := os.Stat(file)
 		if err != nil {
-			log.Printf("Stat error on %s: %s", event.file, err)
+			log.Printf("Stat error on %s: %s", file, err)
 			continue
 		}
 		if event.hasEvent("create") {
@@ -81,7 +88,7 @@ func linker(destdir string, events <-chan *notifyEvent) {
 					log.Printf("Error creating dir: %s", err)
 				}
 			} else {
-				err := os.Link(event.file, destdir)
+				err := os.Link(file, destdir)
 				if err != nil {
 					log.Printf("Error linking file: %s", err)
 				}
@@ -89,7 +96,7 @@ func linker(destdir string, events <-chan *notifyEvent) {
 		}
 		if event.hasEvent("modify") {
 			if fi.IsDir() {
-				cmd := exec.Command("cp", "-al", event.file, destdir)
+				cmd := exec.Command("cp", "-al", file, destdir)
 				cmd.Run()
 			} else {
 				// File modified.  Already linked, so do nothing.
